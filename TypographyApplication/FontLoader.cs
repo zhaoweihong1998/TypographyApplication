7eb47ac58f75dcc4fd98b81c934774c15c472428
zhaoweihong1998 <61811550+zhaoweihong1998@users.noreply.github.com>
2022-06-28 10:36:20 +0800

    git init

---
+++ TypographyApplication/FontLoader.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using Typography.OpenFont;
+using Typography.TextLayout;
+namespace FontLoader
+{
+    public class FontLoader
+    {
+        public static Typeface LoadFont(string path)
+        {
+            var file = new FileStream(path, FileMode.Open);
+            var reader = new OpenFontReader();
+            Typeface _ofTypeface = reader.Read(file);
+            return _ofTypeface;
+        }
+
+        public static GlyphLayout GetGlyphLayout(Typeface typeface, char[] chars, bool gpos, bool gsub)
+        {
+            GlyphLayout layout = new GlyphLayout();
+            layout.EnableGpos = gpos;
+            layout.EnableGsub = gsub;
+            layout.Typeface = typeface;
+            layout.Layout(chars, 0, chars.Length);
+
+            return layout;
+        }
+
+
+       /* static public void Main()
+        {
+            var typeface = LoadFont("./Fonts/Lobster-Regular.ttf");
+            var layout = GetGlyphLayout(typeface, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray(),true,true);
+            System.Console.WriteLine("done");
+            foreach (var item in layout.GetUnscaledGlyphPlanIter())
+            {
+                System.Console.WriteLine(item.ToString());
+            }
+        }*/
+    }
+}
---
+++ TypographyApplication/Libs/myGlyohTranslator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Typography.OpenFont
+{
+    class myGlyohTranslator : IGlyphTranslator
+    {
+        public void BeginRead(int contourCount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CloseContour()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Curve3(float x1, float y1, float x2, float y2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Curve4(float x1, float y1, float x2, float y2, float x3, float y3)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EndRead()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LineTo(float x1, float y1)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MoveTo(float x0, float y0)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
